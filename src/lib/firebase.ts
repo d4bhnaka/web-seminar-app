2727a267b799359473b943f5c0e7875fea376175
DESIGN4B <staff@design4b.co.jp>
2020-12-10 17:13:47 +0900

    add dashboard

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyALHHoda7XmZ_Ui9Odq4dQk4omo3xsG9Zg",
-  authDomain: "webseminar-20cd6.firebaseapp.com",
-  projectId: "webseminar-20cd6",
-  storageBucket: "webseminar-20cd6.appspot.com",
-  messagingSenderId: "637731147583",
-  appId: "1:637731147583:web:af412e7e57646e57aa0d8a",
-  measurementId: "G-55TTW6MYDW"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 if (!firebase.apps.length) {
--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -1,8 +1,64 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import styles from "../styles/DashboardScreen.module.css";
+import ReactPlayer from "react-player/lazy";
 
 type Props = {};
 
-export const DashboardScreen: React.FC<Props> = ({}: Props) => {
-  return <></>;
+export const DashboardScreen: React.FC<Props> = () => {
+  useEffect(() => {
+    console.log(process.env.REACT_APP_VIMEO_CLIENT_SECRETS);
+  }, []);
+
+  let theme = createMuiTheme({
+    palette: {
+      primary: {
+        light: "#63ccff",
+        main: "#009be5",
+        dark: "#006db3",
+      },
+    },
+    typography: {
+      h5: {
+        fontWeight: 500,
+        fontSize: 26,
+        letterSpacing: 0.5,
+      },
+    },
+    shape: {
+      borderRadius: 8,
+    },
+    props: {
+      MuiTab: {
+        disableRipple: true,
+      },
+    },
+    mixins: {
+      toolbar: {
+        minHeight: 48,
+      },
+    },
+  });
+  return (
+    <ThemeProvider theme={theme}>
+      <div className={styles.root}>
+        <div className={styles.app}>
+          <main className={styles.main}>
+            <h1>Dashboard</h1>
+            <ReactPlayer
+              url="https://vimeo.com/388389795"
+              controls={true}
+              config={{
+                vimeo: {
+                  playerOptions: {
+                    autoplay: true,
+                  },
+                },
+              }}
+            />
+          </main>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 };
